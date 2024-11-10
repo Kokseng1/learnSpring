bccece57b2b2bc9391eeb8a0022397116b05287d
kokseng1 <ngkokseng02@gmail.com>
2024-11-10 23:42:13 +0800

    package : init backend

---
+++ apps/learn-spring-backend/src/main/java/com/learnSpring/learnSpring/LearnSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learnSpring.learnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBackendApplication.class, args);
+	}
+
+}
