3ed4b166c596d4228432c15d9e20adfdeb47b1cf
kokseng1 <ngkokseng02@gmail.com>
2024-11-11 21:35:20 +0800

    fix : able to start backend with nx serve todo : finish up authentication

---
+++ apps/learn-spring-backend/src/main/java/com/learnSpring/learnSpring/shared/authentication/infrastructure/primary/KindeJwtAuthenticationConverter.java
@@ -0,0 +1,28 @@
+package com.learnSpring.learnSpring.shared.authentication.infrastructure.primary;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import io.micrometer.common.lang.NonNull;
+
+public class KindeJwtAuthenticationConverter implements org.springframework.core.convert.converter.Converter<Jwt, AbstractAuthenticationToken>{
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt source) {
+        return new JwtAuthenticationToken(source, 
+            Stream.concat(new JwtGrantedAuthoritiesConverter().convert(source).stream(), extractResourceRoles(source).stream()).collect(Collectors.toSet()));
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        return AuthenticatedUser.extractRolesFromToken(jwt).stream()
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toSet());
+    }
+}
